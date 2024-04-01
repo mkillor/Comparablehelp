b4212c18b7eda164a87d8fb90ebd6d72557b12c3
mak1115 <mak1115@swcsd.us>
2024-04-01 10:57:44 -0400

    Removed Enums for sorting

--- src/com/company/killoran/Main.java
+++ src/com/company/killoran/Main.java
@@ -4,11 +4,11 @@ import java.util.*;
 class Main {
     public static void main(String[] args) {
 
-        Games game1 = new Games(GameSystem.XBOX,"Halo 2");
-        Games game2 = new Games(GameSystem.SWITCH,"Animal Crossing");
-        Games game3 = new Games(GameSystem.PS5,"Spiderman - Miles Morales");
-        Games game4 = new Games(GameSystem.XBOX,"Halo");
-        Games game5 = new Games(GameSystem.PC,"Helldivers2");
+        Games game1 = new Games("XBOX","Halo 2");
+        Games game2 = new Games("SWITCH","Animal Crossing");
+        Games game3 = new Games("PS5","Spiderman - Miles Morales");
+        Games game4 = new Games("XBOX","Halo");
+        Games game5 = new Games("PC","Helldivers2");
 
         ArrayList<Games> myInventory = new ArrayList<>();
         myInventory.add(game1);
@@ -26,13 +26,13 @@ class Main {
 }
 
 
-enum GameSystem {XBOX, PS5, SWITCH, PC};
+
 
 class Games implements Comparable<Games> {
-    private GameSystem system;
+    private String system;
     private String title;
 
-    public Games(GameSystem system, String title) {
+    public Games(String system, String title) {
         this.system = system;
         this.title = title;
     }
@@ -54,9 +54,7 @@ class Games implements Comparable<Games> {
 
         //type below first---
         if(this.system==other.system){
-            return 0;
-            //--alter above to sort by title if equal
-            // --return this.title.compareTo(other.title);
+            return this.title.compareTo(other.title);
         }
         else {
             return this.system.compareTo(other.system);
